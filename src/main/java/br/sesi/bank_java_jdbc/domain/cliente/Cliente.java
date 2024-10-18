101675ff66b6c7cbd8c66e6378fdf9962a5115da
unknown <davitavaressilva2007@gmail.com>
2024-10-18 09:36:39 -0300

    Tarefa Diogo-18/10

--- src/main/java/br/sesi/bank_java_jdbc/domain/cliente/Cliente.java
+++ src/main/java/br/sesi/bank_java_jdbc/domain/cliente/Cliente.java
@@ -1,24 +1,50 @@
 package br.sesi.bank_java_jdbc.domain.cliente;
 
+import java.util.Objects;
+
 public class Cliente {
     private String nome;
     private String cpf;
     private String email;
 
-    public Cliente(DadosCadastroCliente dados){
+    public Cliente(DadosCadastroCliente dados) {
         this.nome = dados.nome;
         this.email = dados.email;
         this.cpf = dados.cpf;
     }
 
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public String getCpf(){
+
+    public String getCpf() {
         return cpf;
     }
-    public String getEmail(){
+
+    public String getEmail() {
         return email;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return cpf.equals(cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + nome + '\'' +
+                ", email='" + nome + '\'' +
+                '}';
+    }
 }
+
